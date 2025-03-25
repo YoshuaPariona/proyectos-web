b9a9808ec255df96d1f6dec6869f34a06a955a51
Yoshua <loganpariona.1207@gmail.com>
2025-03-24 19:08:33 -0500

    Implementar: terminar lógica del juego

--- react-02-michi/src/App.jsx
+++ react-02-michi/src/App.jsx
@@ -19,33 +19,79 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURN.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a];
+      }
+    }
+    return null;
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null));
+    setTurn(TURN.X);
+    setWinner(null);
+  }
+
+  const checkEndGame = (newBoard) => {
+    return newBoard.every((square) => square !== null);
+  }
 
   const updateBoard = (index) => {
+    if(board[index] || winner) return;
+
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
 
     const newTurn = turn === TURN.X ? TURN.O : TURN.X;
     setTurn (newTurn);
-    console.log(newTurn);
+    
+    const newWinner = checkWinner(newBoard);
+    if(newWinner) {
+      setWinner(newWinner);
+
+    }else if(checkEndGame(newBoard)){
+      setWinner(false);
+    }
   }
 
   return (
     <main className="board">
       <h1>Michi</h1>
+      <button onClick={resetGame}>Reset del juego</button>
       <section className="game">
         {
-          board.map((_, index) => {
+          board.map((square, index) => {
             return (
               <Square
                 key={index}
                 index={index}
                 updateBoard={updateBoard}
               >
-                {board[index]}
+                {square}
               </Square>
             )
           })
@@ -60,6 +106,31 @@ function App() {
         </Square>
         
       </section>
+      
+        {
+          winner !== null && (
+            <section className='winner'>
+              <div className='text'>
+                <h2>
+                  {
+                    winner === false
+                    ? 'Empate'
+                    : 'Ganó: '
+                  }
+                </h2>
+                <header>
+                  {winner && <Square>{winner}</Square>}
+                </header>
+                <footer>
+                  <button onClick={resetGame}>Empezar de nuevo</button>
+                </footer>
+              </div>
+            </section>
+          )
+
+        }
+
+      
     </main>
   )
 }
